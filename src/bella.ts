22da2cc381e12876476f16bd008df5932d8ec19f
panayiotishalios <panayiotis.halios@gmail.com>
2023-05-01 21:12:40 -0700

    Initial commit

---
+++ src/bella.ts
@@ -0,0 +1,317 @@
+import util from 'util';
+
+// Defining the types
+type PredefinedFunc = (...args: number[]) => Value
+type FuncDec = [Identifier[], Expression]
+type Value = number | boolean | Value[] | PredefinedFunc | FuncDec
+
+// helper function to determine if we have a function declaration instead of just a Value[]
+function isFuncDec(arg: any): arg is FuncDec {
+    return Array.isArray(arg) && arg.length === 2 && Array.isArray(arg[0]) && typeof arg[1] === 'object' && arg[1] !== null;
+}
+
+// Only one memory store for all the variables.
+// Global and local variables are stored in the same memory
+export let memory = new Map<string, Value>()
+memory.set("π", Math.PI);
+memory.set("sqrt", (x: number) => Math.sqrt(x));
+memory.set("sin", (x: number) => Math.sin(x));
+memory.set("cos", (x: number) => Math.cos(x));
+memory.set("exp", (x: number) => Math.exp(x));
+memory.set("ln", (x: number) => Math.log2(x));
+memory.set("hypot", (x: number) => Math.hypot(x));
+
+interface Statement {
+    interpret(): void
+}
+
+interface Expression {
+    interpret(): Value
+}
+
+export class Numeral implements Expression {
+    constructor(public value: number) { }
+
+    interpret(): Value {
+        return this.value
+    }
+}
+
+export class BooleanLiteral implements Expression {
+    constructor(public value: boolean) { }
+
+    interpret(): Value {
+        return this.value
+    }
+}
+
+export class Identifier implements Expression {
+    constructor(public name: string) { }
+
+    interpret(): Value {
+        const value = memory.get(this.name)
+        if (value === undefined) {
+            throw new Error('Variable not declared')
+        } else {
+            return value
+        }
+    }
+}
+
+export class UnaryExpression implements Expression {
+    constructor(public operator: string, public argument: Expression) { }
+
+    interpret(): Value {
+        switch (this.operator) {
+            case '-':
+                return -this.argument.interpret()
+            case '!':
+                return !this.argument.interpret()
+            default:
+                throw new Error('Invalid unary operator')
+        }
+    }
+}
+
+export class BinaryExpression implements Expression {
+    constructor(public operator: string, public left: Expression, public right: Expression) { }
+    interpret(): Value {
+        const mathOperatos = ['+', '-', '*', '/', '%', '**']
+        if (mathOperatos.includes(this.operator)) {
+            const left = this.left.interpret()
+            const right = this.right.interpret()
+
+            if (typeof left !== 'number' || typeof right !== 'number') {
+                throw new Error(`Invalid operand types for binary operator ${this.operator}`)
+            } else {
+                switch (this.operator) {
+                    case '+':
+                        return left + right
+                    case '-':
+                        return left - right
+                    case '*':
+                        return left * right
+                    case '/':
+                        return left / right
+                    case '%':
+                        return left % right
+                    case '**':
+                        return left ** right
+                    default:
+                        throw new Error(`Invalid binary operator ${this.operator}`)
+                }
+            }
+        } else {
+            switch (this.operator) {
+                case '==':
+                    return this.left.interpret() === this.right.interpret()
+                case '!=':
+                    return this.left.interpret() !== this.right.interpret()
+                case '<':
+                    return this.left.interpret() < this.right.interpret()
+                case '<=':
+                    return this.left.interpret() <= this.right.interpret()
+                case '>':
+                    return this.left.interpret() > this.right.interpret()
+                case '>=':
+                    return this.left.interpret() >= this.right.interpret()
+                case '&&':
+                    return this.left.interpret() && this.right.interpret()
+                case '||':
+                    return this.left.interpret() || this.right.interpret()
+                default:
+                    throw new Error(`Invalid binary operator ${this.operator}`)
+            }
+        }
+    }
+}
+
+export class CallExpression implements Expression {
+    constructor(public name: Identifier, public args: Expression[]) { }
+    interpret(): Value {
+        // Checking if the expressions 
+        const func = this.name.interpret()
+        // Check if the callee is either a predefined function or a user defined function
+        if (typeof func === "function") {
+            // Evaluating the arguments
+            const evaluatedArgs = this.args.map((a) => a.interpret());
+            for (const param in evaluatedArgs) {
+                if (typeof param !== "number") {
+                    throw new Error("Invalid parameter type");
+                }
+            }
+            // Can explicitly cast to number[] because of the check above
+            return func(...evaluatedArgs as number[]);
+        } else if (Array.isArray(func) && func.length === 2) {
+            const [parameters, expression] = func as FuncDec
+
+            // Checking if the number of arguments matches the number of parameters
+            if (parameters.length !== this.args.length) {
+                throw new Error("Invalid number of arguments");
+            }
+
+            // Create a deep copy of the memory
+            const memoryCopy = new Map<string, Value>(memory)
+            for (let i = 0; i < parameters.length; i++) {
+                memory.set(parameters[i].name, this.args[i].interpret())
+            }
+
+            const evaluatedExpression = expression.interpret()
+            // Setting the memory back
+            memory = new Map<string, Value>(memoryCopy)
+            return evaluatedExpression
+        } else {
+            throw new Error("Invalid function call");
+        }
+    }
+}
+
+export class ConditionalExpression implements Expression {
+    constructor(public test: Expression, public consequent: Expression, public alternate: Expression) { }
+    interpret(): Value {
+        return this.test.interpret() ? this.consequent.interpret() : this.alternate.interpret()
+    }
+}
+
+export class ArrayLiteral implements Expression {
+    constructor(public elements: Expression[]) { }
+    interpret(): Value {
+        return this.elements.map(element => element.interpret())
+    }
+}
+
+export class SubscriptExpression implements Expression {
+    constructor(public array: Expression, public subscript: Expression) { }
+    
+    interpret(): Value {
+        const arrayValue = this.array.interpret()
+        const subscriptValue = this.subscript.interpret()
+        if (typeof subscriptValue !== 'number') {
+            throw new Error('Subscript must be a number')
+        } else if (!Array.isArray(arrayValue)) {
+            throw new Error('Subscripted value must be an array')
+        } else if (subscriptValue < 0 || subscriptValue >= arrayValue.length) {
+            throw new Error('Subscript is out of range');
+        }
+
+        // Checking if we are subscripting a function
+        if (isFuncDec(arrayValue)) {
+            throw new Error('Cannot subscript a function')
+        }
+
+        return arrayValue[subscriptValue]
+    }
+}
+
+export class VariableDeclaration implements Statement {
+    constructor(public id: Identifier, public expression: Expression) { }
+
+    interpret(): void {
+        if (memory.has(this.id.name)) {
+            throw new Error('Variable already declared')
+        }
+        memory.set(this.id.name, this.expression.interpret())
+    }
+}
+
+export class Assignment implements Statement {
+    constructor(public id: Identifier, public expression: Expression) { }
+
+    interpret(): void {
+        const value = memory.get(this.id.name)
+        
+        if (value === undefined) {
+            throw new Error('Variable not declared')
+        }
+
+        if (typeof value === 'object' || typeof value === 'function') {
+            throw new Error('Can only assign to variables')
+        }
+
+        // Checking that we are trying to assign a value of the same type
+        // const evaluatedExpression = this.expression.interpret()
+
+        // if (typeof value !== typeof evaluatedExpression) {
+        //     throw new Error('Cannot assign to variable of different type')
+        // }
+
+        memory.set(this.id.name, this.expression.interpret())
+    }
+}
+
+export class FunctionDeclation implements Statement {
+    constructor(public id: Identifier, public params: Identifier[], public expression: Expression) { }
+
+    interpret(): void {
+        if (memory.has(this.id.name)) {
+            throw new Error('Function already declared')
+        }
+        memory.set(this.id.name, [this.params, this.expression])
+    }
+}
+
+export class PrintStatement implements Statement {
+    constructor(public expression: Expression) { }
+
+    interpret(): void {
+        console.log(this.expression.interpret())
+    }
+}
+
+export class WhileStament implements Statement {
+    constructor(public test: Expression, public block: Block) { }
+
+    interpret(): void {
+        while (this.test.interpret()) {
+            this.block.interpret()
+        }
+    }
+}
+
+export class Block {
+    constructor(public statements: Statement[]) { }
+
+    interpret() {
+        for (let statement of this.statements) {
+            statement.interpret()
+        }
+    }
+}
+
+export class Program {
+    constructor(public block: Block) { }
+
+    interpret() {
+        memory.clear()
+        return this.block.interpret()
+    }
+}
+
+export default function interpret(program: Program): void {
+    program.interpret()
+}
+
+const test: Program = new Program(
+    new Block([new PrintStatement(new Numeral(5))])
+)
+
+// Testing unary and binary expressions
+const test2: Program = new Program(
+    new Block([
+        new PrintStatement(new UnaryExpression('-', new Numeral(5))),
+        new PrintStatement(new BinaryExpression('*', new Numeral(5), new Numeral(5))),
+    ])
+)
+
+// Testing variable declaration and assignment
+const test3 = new Program(
+    new Block([
+        new VariableDeclaration(new Identifier('x'), new Numeral(5)),
+        new VariableDeclaration(new Identifier('y'), new Numeral(10)),
+        new PrintStatement(new BinaryExpression('*', new Identifier('x'), new Identifier('y'))),
+    ])
+)
+
+// console.log(sample)
+// console.log(util.inspect(sample2, { depth: null }))
+// interpret(test2)
---
+++ test/bella.spec.ts
@@ -0,0 +1,106 @@
+import interpret, { memory, VariableDeclaration, Identifier, Numeral,
+     BooleanLiteral, Assignment, 
+     FunctionDeclation, CallExpression, UnaryExpression, BinaryExpression } from "../src/bella.js";
+import { equal } from "assert";
+
+// We will be using the bella memory to perfom our assertions
+describe("Bella", () => {
+
+    // Reset the memory after every test
+    afterEach(() => {
+        memory.clear()
+    })
+
+    it("should save variables in memory", () => {
+        const x = new VariableDeclaration(new Identifier('x'), new Numeral(10))
+        const xy = new VariableDeclaration(new Identifier('yz'), new BooleanLiteral(false))
+        x.interpret()
+        xy.interpret()
+        equal(memory.get('x'), 10)
+        equal(memory.get('yz'), false)
+    });
+
+    it("should be able to assign to variables", () => {
+        const x = new VariableDeclaration(new Identifier('x'), new Numeral(10))
+        x.interpret()
+        equal(memory.get('x'), 10)
+        const xAssignment = new Assignment(new Identifier('x'), new Numeral(20))
+        xAssignment.interpret()
+        equal(memory.get('x'), 20)
+    });
+
+    it("should not be able to assign to variables that are not declared", () => {
+        try {
+            const xAssignment = new Assignment(new Identifier('x'), new Numeral(20))
+            xAssignment.interpret()
+        } catch (error: any) {
+            equal(error.message, "Variable not declared")
+        }
+    });
+
+    it("should be able to assign values of different types to variables", () => {
+        new VariableDeclaration(new Identifier('x'), new Numeral(10)).interpret()
+        new Assignment(new Identifier('x'), new BooleanLiteral(false)).interpret()
+
+        equal(memory.get('x'), false)
+    })
+
+    it("should be able to declare a function and call it", () => {
+        const funcDec = new FunctionDeclation(new Identifier("timesTwo"), 
+            [new Identifier("x")], new BinaryExpression("*", 
+                new Identifier("x"), new Numeral(2)))
+        funcDec.interpret()
+        const funcCall = new CallExpression(new Identifier("timesTwo"), [new Numeral(10)])
+        const value = funcCall.interpret()
+        equal(value as number, 20)
+    })
+
+    it("should be able to not modify global memory in a function call", () => {
+        
+        new VariableDeclaration(new Identifier('x'), new Numeral(10)).interpret()
+        
+        // Define func with param x
+        const funcDec = new FunctionDeclation(new Identifier("timesTwo"), 
+            [new Identifier("x")], new BinaryExpression("*", 
+                new Identifier("x"), new Numeral(2)))
+        funcDec.interpret()
+        // call func with param 200
+        const funcCall = new CallExpression(new Identifier("timesTwo"), [new Numeral(200)])
+        funcCall.interpret()
+        
+        // Verify that X is still 10
+        equal(memory.get('x'), 10)
+
+    })
+
+    it("should be able to call a predefined function", () => {
+        const funcCall = new CallExpression(new Identifier("cos"), [new Numeral(10)])
+        const value = funcCall.interpret()
+        equal(value as number, Math.cos(10))
+    })
+
+    it("Print statements", () => {
+
+    })
+
+    it("While statements", () => {
+
+    })
+
+    it("Conditional Expressions", () => {
+
+    })
+
+    it("Unary Expressions", () => {
+        // const negation = new UnaryExpression("!", )
+        // const negative = new UnaryExpression("-", )
+    })
+
+    it("Binary Expressions", () => {
+        
+    })
+})
+
+// describe("Bella Statements", () => {
+
+// })
